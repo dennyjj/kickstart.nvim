80a24259527de80143470c6395a36ec2ffc1f287
Dennis Chan <dennisming2012@gmail.com>
2025-07-17 18:00:41 +0800

    remove comment

--- init.lua
+++ init.lua
@@ -15,7 +15,6 @@ vim.api.nvim_create_autocmd('FileChangedShellPost', {
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = 'l'
 
